d92326c6381333df3658b1264805df19a3ba44e7
Liangce Li <liliangce316@gmail.com>
2023-05-18 16:00:44 +0930

    ko

--- klargest.cpp
+++ klargest.cpp
@@ -1,18 +1,6 @@
-#include <vector>
-#include <queue>
-#include <iostream>
-using namespace std;
 
-/*class klargest{
 
-    public:
-    klargest();
-    int kth_largest(vector<int> values, int k);
-};
-
-klargest :: klargest(){}*/
-
-int kth_largest(vector<int> values, int k){ //k indicates the k'th larges.  For example, if k is 2, then we return the second largest, if k is 5, we return the 5th largest.
+/*int kth_largest(vector<int> values, int k){ //k indicates the k'th larges.  For example, if k is 2, then we return the second largest, if k is 5, we return the 5th largest.
     int target;
 
     priority_queue<int> maxpq;
@@ -24,4 +12,24 @@ int kth_largest(vector<int> values, int k){ //k indicates the k'th larges.  For 
         }
         target=maxpq.top();
         return target;
+}*/
+
+#include <queue>
+#include <vector>
+
+int kth_largest(std::vector<int> values, int k) {
+    std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
+
+    // add elements to the priority queue
+    for (int val : values) {
+        pq.push(val);
+        // if the priority queue has more than k elements, remove the smallest element
+        if (pq.size() > k) {
+            pq.pop();
+        }
+    }
+
+    // the kth largest element is at the top of the priority queue
+    return pq.top();
 }
+
